29cc2d179f6609040711898022e51d9a12d4e750
Nguyễn Bảo Anh <bbb2651999@gmail.com>
2022-10-22 13:20:04 +0700

    origin

--- src/main/java/com/example/doctorcare/api/controller/UserController.java
+++ src/main/java/com/example/doctorcare/api/controller/UserController.java
@@ -3,12 +3,14 @@ package com.example.doctorcare.api.controller;
 import com.example.doctorcare.api.entity.UserEntity;
 import com.example.doctorcare.api.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/")
 public class UserController {
