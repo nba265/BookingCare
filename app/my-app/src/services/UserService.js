2bef6b2cde76b83ea2a4bbf320481d0425fb8ca2
Nguyễn Bảo Anh <bbb2651999@gmail.com>
2022-10-22 14:41:18 +0700

    ahuybede

--- app/my-app/src/services/UserService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const USER_REST_API_URL='http://localhost:8080/api/users';
-class UserService{
-    getUsers(){
-        axios.get(USER_REST_API_URL);
-    }
-}
-export default new UserService();
