c9b48f8dba780d71d6894408b83c34708cd9c7de
HarryNguyen1712 <anhhuynguyen55@gmail.com>
2022-10-26 22:47:10 +0700

    add mapping

--- src/main/java/com/example/doctorcare/api/controller/DoctorController.java
+++ src/main/java/com/example/doctorcare/api/controller/DoctorController.java
@@ -8,16 +8,14 @@ import com.example.doctorcare.api.utilis.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 
 @RestController
+@RequestMapping("api/doctor")
 public class DoctorController {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
