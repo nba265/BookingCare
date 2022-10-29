404102cf04a8a65b8b5654dff63d373a1f3f896e
Nguyễn Bảo Anh <bbb2651999@gmail.com>
2022-10-29 21:19:49 +0700

    update signin

--- src/main/java/com/example/doctorcare/api/domain/dto/request/SignupRequest.java
+++ src/main/java/com/example/doctorcare/api/domain/dto/request/SignupRequest.java
@@ -32,8 +32,4 @@ public class SignupRequest {
     @Size(min = 6, max = 40)
     private String password;
 
-    @NotBlank
-    @Size(min = 6, max = 40)
-    private String rePassword;
-
 }
--- src/main/java/com/example/doctorcare/api/domain/dto/response/JwtResponse.java
+++ src/main/java/com/example/doctorcare/api/domain/dto/response/JwtResponse.java
@@ -1,64 +1,29 @@
 package com.example.doctorcare.api.domain.dto.response;
 
+import com.example.doctorcare.api.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtResponse {
+
     private String token;
-    private String type = "Bearer";
+
     private Long id;
+
     private String username;
+
     private String email;
-    private List<String> roles;
 
-    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
-        this.token = accessToken;
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.roles = roles;
-    }
+    private String roles;
 
-    public String getAccessToken() {
-        return token;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.token = accessToken;
-    }
-
-    public String getTokenType() {
-        return type;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.type = tokenType;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
 }
