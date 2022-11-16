e07d566c760142dfe3a131d6eed6a9b6b97649b3
Nguyễn Bảo Anh <bbb2651999@gmail.com>
2022-11-16 15:09:13 +0700

    Add getUserByAdmin

--- src/main/java/com/example/doctorcare/api/utilis/RoleConst.java
+++ src/main/java/com/example/doctorcare/api/utilis/RoleConst.java
@@ -1,25 +1,14 @@
 package com.example.doctorcare.api.utilis;
 
-import com.example.doctorcare.api.enums.Role;
-
 public class RoleConst {
 
     public static String getRoleConst(String role) {
-        String returnRole = "";
-        switch (role) {
-            case "ROLE_ADMIN":
-                returnRole = "admin";
-                break;
-            case "ROLE_MANAGER":
-                returnRole = "manager";
-                break;
-            case "ROLE_DOCTOR":
-                returnRole = "doctor";
-                break;
-            case "ROLE_USER":
-                returnRole = "user";
-                break;
-        }
-        return returnRole;
+        return switch (role) {
+            case "ROLE_ADMIN" -> "admin";
+            case "ROLE_MANAGER" -> "manager";
+            case "ROLE_DOCTOR" -> "doctor";
+            case "ROLE_USER" -> "user";
+            default -> "";
+        };
     }
 }
