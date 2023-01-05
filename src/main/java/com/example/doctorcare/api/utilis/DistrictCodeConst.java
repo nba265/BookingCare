9d6e737d8b3c3d85a390ed9e1cdd97cbee562c98
Nguyễn Bảo Anh <bbb2651999@gmail.com>
2023-01-05 21:45:30 +0700

    Update DistrictCodeConst.java

--- src/main/java/com/example/doctorcare/api/utilis/DistrictCodeConst.java
+++ src/main/java/com/example/doctorcare/api/utilis/DistrictCodeConst.java
@@ -1,22 +1,25 @@
 package com.example.doctorcare.api.utilis;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Getter
+@NoArgsConstructor
 public class DistrictCodeConst {
-    public static final  Map<String,String> district = new HashMap<>();
-
-    DistrictCodeConst(){
-        district.put("00","");
-        district.put("01","Quận Hải Châu");
-        district.put("02","Quận Thanh Khê");
-        district.put("03","Quận Sơn Trà");
-        district.put("04","Quận Ngũ Hành Sơn");
-        district.put("05","Quận Liên Chiểu");
-        district.put("06","Quận Hòa Vang");
-        district.put("07","Quận Cẩm Lệ");
-    }
+    public static final Map<String,String> district = new HashMap<String, String>(){
+        {
+            put("00", "");
+            put("01", "Quận Hải Châu");
+            put("02", "Quận Thanh Khê");
+            put("03", "Quận Sơn Trà");
+            put("04", "Quận Ngũ Hành Sơn");
+            put("05", "Quận Liên Chiểu");
+            put("06", "Quận Hòa Vang");
+            put("07", "Quận Cẩm Lệ");
+        }
+    };
 
 }
